b832395ef6550d1a6b26059863bcb5958850917e
Anthony Rubbo <anthony.rubbo@gmail.com>
2021-11-11 11:33:07 -0500

    refactored state and calcs in App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,82 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      result: null,
+      numArray: ''
+    }
+  }
+
+  handleNumInput = evt => {
+    let nums = evt.target.value.split(',')
+    this.setState({
+      numArray: nums,
+    })
+  }
+
+  handleInvalidInput = () => {
+    this.setState({
+      result: "Invalid input."
+    })
+  }
+
+  handleSum = () => {
+    const {numArray} = this.state
+    this.setState({
+      result: numArray.reduce((a, b) => Number(a) + Number(b)),
+      numArray: ''
+    })
+  }
+
+  handleAverage = () => {
+    const {numArray} = this.state
+    this.setState({
+      result: numArray.reduce((a, b) => Number(a) + Number(b)) / numArray.length,
+      numArray: ''
+    })
+  }
+
+  handleMode = () => {
+    const {numArray} = this.state
+    let numObj = {}
+    let most = 1
+    for(let num of numArray){
+      if(!numObj[num]){
+        numObj[num] = 1
+      } else {
+        numObj[num] += 1
+      }
+    }
+
+    for(let num in numObj){
+      if(numObj[num] > most){
+        most = num
+      }
+    }
+
+    this.setState({
+      result: most,
+      numArray: ''
+    })
+  }
+
   render() {
+    console.log(this.state.numArray)
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form 
+        handleNumInput={this.handleNumInput} 
+        handleInvalidInput={this.handleInvalidInput}
+        handleAverage={this.handleAverage}
+        handleSum={this.handleSum}
+        handleMode={this.handleMode}
+        numArray={this.state.numArray}
+        />
         <section id="result">
-          <p></p>
+          <p>{this.state.result}</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -2,21 +2,12 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
-      numArray: [],
       operation: '',
-      result: null,
       isValid: null
     }
-  }
-
-  handleNumInput = evt => {
-    let nums = evt.target.value.split(',')
-    this.setState({
-      numArray: nums,
-    })
   }
 
   handleOperation = evt => {
@@ -26,61 +17,42 @@ class Form extends React.Component {
   }
   
   handleSubmit = evt => {
-    const { numArray, operation } = this.state
+    const { operation } = this.state
     evt.preventDefault()
     if(operation === ''){
+      this.props.handleInvalidInput()
       this.setState({
-        result: "Invalid input.",
         isValid: "error"
       })
     } else if(operation === 'sum'){
+      this.props.handleSum()
       this.setState({
-        result: numArray.reduce((a, b) => Number(a) + Number(b)),
         isValid: null,
-        numArray: [],
         operation: ''
       })
     } else if (operation === 'average'){
+      this.props.handleAverage()
       this.setState({
-        result: numArray.reduce((a, b) => Number(a) + Number(b)) / numArray.length,
         isValid: null,
-        numArray: [],
         operation: ''
       })
     } else {
-      let numObj = {}
-      let most = 1
-      for(let num of numArray){
-        if(!numObj[num]){
-          numObj[num] = 1
-        } else {
-          numObj[num] += 1
-        }
-      }
-
-      for(let num in numObj){
-        if(numObj[num] > most){
-          most = num
-        }
-      }
-
+      this.props.handleMode()
       this.setState({
-        result: most,
         isValid: null,
-        numArray: [],
         operation: ''
       })
     }
   }
   
   render() {
-    const { numArray, operation, result, isValid } = this.state 
-    console.log(numArray, operation, result)
+    const {  operation, isValid } = this.state 
+    const { handleNumInput } = this.props
 
     return (
       <>
       <form onSubmit={this.handleSubmit}>
-        <input className={isValid} id="values" name="values" type="text" onChange={this.handleNumInput} value={numArray}/>
+        <input className={isValid} id="values" name="values" type="text" value={this.props.numArray} onChange={(evt) => handleNumInput(evt)} />
         <select className={isValid} onChange={this.handleOperation} id="operation" name="operation" value={operation}>
           <option value=""></option>
           <option value="sum">sum</option>
@@ -89,10 +61,8 @@ class Form extends React.Component {
         </select>
         <button type="submit">Calculate</button>
       </form>
-      <p id="result">{this.state.result}</p>
       </>
     );
   }
 }
-
 export default Form;
